38d75da674341163b0f844256371fbbfd1e500b2
polatengin <enpolat@microsoft.com>
2021-06-23 17:07:31 +0300

    update extension method for bubbletea is defined

--- main.go
+++ main.go
@@ -76,5 +76,37 @@ func getListOfBlogPosts() []BlogPost {
 func (m model) Init() tea.Cmd {
 	return nil
 }
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			tea.Quit()
+
+			e, ok := m.selected[m.cursor]
+
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+
+			log.Printf("\n\n---\n\n%v\n\n---\n\n", e)
+		case "esc":
+		}
+	}
+
+	return m, nil
+}
 func main() {
 }
