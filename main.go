cfb49c3796af619dcbf706504d204c9e1beb059f
polatengin <enpolat@microsoft.com>
2021-06-23 17:02:09 +0300

    main method defined

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"net/http"
+
+	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/glow/utils"
+
+	tea "github.com/charmbracelet/bubbletea"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+}
